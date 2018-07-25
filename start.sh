3eac78f565060a668b605a2852702d1216fcff8c
Kyle Welsby <kyle@mekyle.com>
2018-07-25 14:46:36 +0100

    Use NPM concurrently to run Gulp and Fractal
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 BLUE='\033[0;96m'
 NC='\033[0m'
 echo -e "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
-echo -e "┃                     ${BLUE}Building Co-op design system${NC}                      ┃"
+echo -e "┃                     ${BLUE}Buidling Co-op design system${NC}                      ┃"
+echo -e "┃                     DEPRECATED USE # 'npm start'                      ┃"
 echo -e "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
-fractal start --watch --sync &
-gulp
+npm start
