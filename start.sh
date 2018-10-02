de97b504919058c6910fdca6ce1e3a32db6375f8
Matt Tyas <matthewtyas@hotmail.com>
2018-10-02 15:11:41 +0100

    tidying

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-BLUE='\033[0;96m'
-NC='\033[0m'
-echo -e "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
-echo -e "┃                     ${BLUE}Buidling Co-op design system${NC}                      ┃"
-echo -e "┃                     DEPRECATED USE # 'npm start'                      ┃"
-echo -e "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
-npm start
